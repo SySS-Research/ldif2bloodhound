16b95355f27fb30669ebb1e6f2d756e9bc61e418
Adrian Vollmer <adrian.vollmer@syss.de>
2026-02-09 16:25:33 +0100

    formatting

--- ldif2bloodhound/parser.py
+++ ldif2bloodhound/parser.py
@@ -267,11 +267,6 @@ def convert_sid(sid):
     # The sub-ids include the Domain SID and the RID representing the object
     # '<I' is the format string. "<" specifies that the bytes are little-endian. "I" specifies "unsigned int".
     # This decodes in 4 byte chunks starting from the 8th byte until the last byte
-    sub_ids = [
-        struct.unpack("<I", sid[8 + 4 * i : 12 + 4 * i])[0]
-        for i in range(number_of_sub_ids)
-    ]
+    sub_ids = [struct.unpack("<I", sid[8 + 4 * i : 12 + 4 * i])[0] for i in range(number_of_sub_ids)]
 
-    return "S-{0}-{1}-{2}".format(
-        revision, iav, "-".join([str(sub_id) for sub_id in sub_ids])
-    )
+    return "S-{0}-{1}-{2}".format(revision, iav, "-".join([str(sub_id) for sub_id in sub_ids]))
