79485840f46a7863dec9db387f96e56aa0f6f252
Adrian Vollmer <adrian.vollmer@syss.de>
2026-02-09 16:10:13 +0100

    Avoid pwnlib dependency
    This was only for fixing the logger

--- ldif2bloodhound/__main__.py
+++ ldif2bloodhound/__main__.py
@@ -31,17 +31,20 @@ Based on ADExplorerSnapshot.py. By Adrian Vollmer, SySS GmbH."""
 def main():
     import logging
 
-    import pwnlib
     from adexpsnapshot import ADExplorerSnapshot
     from ldif2bloodhound.parser import LDIFSnapshot
 
-    logging.basicConfig(handlers=[pwnlib.log.console])
-    log = pwnlib.log.getLogger(__name__)
-    log.setLevel(20)
+    try:
+        import pwnlib
+        logging.basicConfig(handlers=[pwnlib.log.console])
+        log = pwnlib.log.getLogger(__name__)
+        log.setLevel(20)
 
-    if pwnlib.term.can_init():
-        pwnlib.term.init()
-    log.term_mode = pwnlib.term.term_mode
+        if pwnlib.term.can_init():
+            pwnlib.term.init()
+        log.term_mode = pwnlib.term.term_mode
+    except ImportError:
+        pass
 
     args = parse_args()
 
